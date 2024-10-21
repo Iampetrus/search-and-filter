395b6395717e5503b5028ab1cc201bd7f44683c1
Iampetrus <pedroalerri@yahoo.es>
2024-10-21 11:53:57 +0200

    ...

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,29 +1,29 @@
-import "./Nav.css";
-import { FiHeart } from "react-icons/fi";
+import './Nav.css';
+import {FiHeart} from 'react-icons/fi' 
 import { HiOutlineUserAdd } from "react-icons/hi";
 import { CiShoppingCart } from "react-icons/ci";
 function Nav() {
   return (
-    <nav>
-      <div className="nav-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Enter your search here"
-        />
-      </div>
-      <div className="profile-container">
-        <a href="#">
-          <FiHeart className="nav-icons" />
-        </a>
-        <a href="#">
-          <CiShoppingCart className="nav-icons" />
-        </a>
-        <a href="#">
-          <HiOutlineUserAdd className="nav-icons" />
-        </a>
-      </div>
-    </nav>
+  <nav>
+    <div>
+      <input 
+        type='text' 
+        className='searc-input' 
+        placeholder='Enter your search here' 
+      />
+    </div>
+  <div className='profile-container'>
+    <a href='#'>
+      <FiHeart className='nav-icons' />
+    </a>
+    <a href='#'>
+      <CiShoppingCart className='nav-icons' />
+    </a>
+    <a href='#'>
+      <HiOutlineUserAdd className='nav-icons' />
+    </a>
+  </div>
+  </nav>
   );
 }
 export default Nav;
