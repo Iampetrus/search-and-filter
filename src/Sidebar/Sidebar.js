c5bf6227ebefacceec66f5548d4870d7c489baa1
Iampetrus <pedroalerri@yahoo.es>
2024-10-22 09:50:02 +0200

    Categories section. Labels and  style for each category

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,13 +1,14 @@
 import Category from './Category/Category';
 import Price from './Price/Price';
 import Colors from './Colors/Colors';
+import { GiShoppingCart } from "react-icons/gi";
 import './Sidebar.css';
 
 function Sidebar() {
   return <>
     <section className='sidebar'>
         <div className='logo-container'>
-            <h1> cart logo</h1>
+            <h1><GiShoppingCart /></h1>
         </div>
         <Category />
         <Price />
