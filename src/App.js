7dd93e217aad7bd279a1fb256cab872592f2e31c
Iampetrus <pedroalerri@yahoo.es>
2024-10-24 11:03:25 +0200

    button filter

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import Navigation from './Navigation/Nav';
 import Products from './Products/Products';
 import Recommended from './Recommended/Recommended';
 import Sidebar from './Sidebar/Sidebar';
+import Card from './components/Card';
 
 // data import
 import products from './db/data';
 
 function App() {
   const[selectedCategory, setSelectedCategory] = useState(null);
-
-  //input filter
   const[query, setQuery] = useState('');
-
+  
+  //input filter
   const handleInputChange = event  => {
     setQuery(event.target.value)
   }
 
-  const filteredItems = products.filter(product => product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+  const filteredItems = products.filter(product =>
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
 );
 
 // radio filter
@@ -30,6 +31,39 @@ const handleChange = event => {
 const handleClick= event => {
   setSelectedCategory(event.target.value)
 }
+
+function filteredData(products, selected, query) {
+  let filteredProducts  = products
+
+  //filtering input items
+  if(query) {
+    filteredProducts = filteredItems
+  }
+  //selected filter
+  if(selected) {
+    filteredProducts= filteredProducts.filter(
+      ({category, color, company, newPrice, title}) =>
+        category === selected ||
+        color === selected ||
+        company  === selected ||
+        newPrice === selected ||
+        title === selected
+    )
+  }
+return filteredProducts.map(({img, title, star, reviews, prevPrice, newPrice}) => (
+  <Card
+    Key={Math.random()}
+    img={img}
+    title={title}
+    star={star}
+    reviews={reviews}
+    newPrice={newPrice}
+    prevPrice={prevPrice}
+    />
+  ));
+}
+
+const result = filteredData(products, selectedCategory, query)
 
   return (
     <>
