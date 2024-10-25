bed09fab3edec8b2a8992efcc0b841d0b1e3ba56
Iampetrus <pedroalerri@yahoo.es>
2024-10-25 13:02:36 +0200

    Price refactoring (Inputs), color and card

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,12 +1,51 @@
 import "./Colors.css";
-import Input from '../../components/Input';
-function Colors() {
+import Input from "../../components/Input";
+function Colors({handleChange}) {
   return (
     <div>
       <h2 className='sidebar-title color-title'>Colors</h2>
-      <Input />
-      <Input />
-      <Input />
+      <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value="" name="test2" />
+        <span className="checkmark all"></span>All
+      </label>
+      <Input 
+      handleChange={handleChange}
+      value='black'
+      title='Black'
+      name='test1'
+      color='black'
+      />
+      <Input 
+      handleChange={handleChange}
+      value='blue'
+      title='Blue'
+      name='test1'
+      color='blue'
+      />
+      <Input 
+      handleChange={handleChange}
+      value='red'
+      title='Red'
+      name='red'
+      color='red'
+      />
+      <Input 
+      handleChange={handleChange}
+      value='green'
+      title='Green'
+      name='green'
+      color='green'
+      />
+      <label className='sidebar-label-container'>
+        <input  ttype='radio' onChange={handleChange} value='white'  name='test1' />
+        <span
+        className='checkmark'
+        style={{
+          background:'white',
+          border:'2px solid black'
+        }}
+        ></span>
+      </label>
     </div>
   );
 }
